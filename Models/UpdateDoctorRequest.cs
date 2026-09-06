4679d99558f9534fdfbf7723310b9e43c0f0710e
Violet <148069072+ViolettaNcl@users.noreply.github.com>
2026-09-06 10:15:04 +0300

    Validate doctor knowledge on update

--- Models/UpdateDoctorRequest.cs
+++ Models/UpdateDoctorRequest.cs
@@ -1,10 +1,20 @@
-﻿namespace DentalClinic.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DentalClinic.Models;
 
 public class UpdateDoctorRequest
 {
+    [StringLength(150, MinimumLength = 1)]
     public string? FullName { get; set; }
+
     public bool? IsActive { get; set; }
+
+    [StringLength(300)]
     public string? Specialization { get; set; }
+
+    [Range(0, 80, ErrorMessage = "Стаж должен быть от 0 до 80 лет")]
     public int? ExperienceYears { get; set; }
+
+    [StringLength(500)]
     public string? Bio { get; set; }
 }
