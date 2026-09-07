27d9d23f0a4fa4bb54f19a4e81793416efacca96
Violet <148069072+ViolettaNcl@users.noreply.github.com>
2026-09-07 12:16:05 +0300

    Add shared frontend password policy validator

---
+++ wwwroot/assets/js/utils/passwordPolicy.js
@@ -0,0 +1,33 @@
+export const PASSWORD_POLICY = Object.freeze({
+    minLength: 8,
+    requireUppercase: true,
+    requireLowercase: true,
+    requireNumber: true,
+    requireSpecial: true
+});
+
+export function validatePassword(password = '') {
+    const value = String(password);
+    const errors = [];
+
+    if (value.length < PASSWORD_POLICY.minLength) {
+        errors.push('password_min_length');
+    }
+    if (PASSWORD_POLICY.requireUppercase && !/[A-Z]/.test(value)) {
+        errors.push('password_uppercase');
+    }
+    if (PASSWORD_POLICY.requireLowercase && !/[a-z]/.test(value)) {
+        errors.push('password_lowercase');
+    }
+    if (PASSWORD_POLICY.requireNumber && !/\d/.test(value)) {
+        errors.push('password_number');
+    }
+    if (PASSWORD_POLICY.requireSpecial && !/[^A-Za-z0-9]/.test(value)) {
+        errors.push('password_special');
+    }
+
+    return {
+        valid: errors.length === 0,
+        errors
+    };
+}
