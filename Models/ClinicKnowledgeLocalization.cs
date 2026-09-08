5a2449d7c6e92046e5bbfc604a0ccca7f07bf393
Violet <148069072+ViolettaNcl@users.noreply.github.com>
2026-09-09 02:56:47 +0300

    Add verified clinic knowledge localization entity

---
+++ Models/ClinicKnowledgeLocalization.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DentalClinic.Models;
+
+[Table("ClinicKnowledgeLocalizations")]
+public class ClinicKnowledgeLocalization
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ClinicKnowledgeItemId { get; set; }
+
+    [Required, StringLength(2)]
+    public string Lang { get; set; } = null!;
+
+    [Required, StringLength(160)]
+    public string Title { get; set; } = null!;
+
+    [Required, StringLength(1200)]
+    public string Content { get; set; } = null!;
+
+    [StringLength(300)]
+    public string? Keywords { get; set; }
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
